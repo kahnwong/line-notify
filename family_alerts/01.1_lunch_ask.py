59ed46ae8c530782a24ae5925b85a02664c76cc6
Karn Wong <karn@karnwong.me>
2024-01-16 12:23:22 +0700

    init commit

---
+++ family_alerts/01.1_lunch_ask.py
@@ -0,0 +1,8 @@
+from family_alerts.utils.message import send_message
+
+
+message = "ถามคนแก่ว่าเที่ยงกินอะไร"
+
+
+r = send_message(message)
+print(r)
---
+++ family_alerts/01.2_lunch_order.py
@@ -0,0 +1,8 @@
+from family_alerts.utils.message import send_message
+
+
+message = "สั่งข้าวเที่ยง"
+
+
+r = send_message(message)
+print(r)
